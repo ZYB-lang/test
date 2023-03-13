58b13094c196d60925ee3491f86290d7af783328
zyb <207239391@qq.com>
2023-03-13 19:05:08 +0800

    Initial commit

---
+++ src/com/zyb/main/main.java
@@ -0,0 +1,14 @@
+package com.zyb.main;
+
+/**
+ * @ClassName main
+ * @Description TODO
+ * @Author ZYB
+ * @Date 2023/3/13 19:04
+ * @Version 1.0
+ **/
+public class main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
